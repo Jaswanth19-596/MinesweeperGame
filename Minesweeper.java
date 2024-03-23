ff087f7a6b58ce343fb58ca95e2e5d57e4c18053
Jaswanth <madhajaswanth@gmail.com>
2024-03-22 23:47:13 -0500

    Functionality Done

--- Minesweeper.java
+++ Minesweeper.java
@@ -1,6 +1,12 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Minesweeper {
+
+    static int mines = 0;
+
+
+
     public static void printMinesweeperBoard(int [][]board) {
 
         // Print the board
@@ -10,6 +16,28 @@ public class Minesweeper {
             }
             System.out.println();
             printSeparator(board[0].length);
+        }
+    }
+
+    public static void printBoard(int [][]board) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                char res = '.';
+                if(board[i][j] == 0){
+                    res = '.';
+                }
+                else if(board[i][j] == 100){
+                    res = '.';
+                }
+                else if(board[i][j] == -1){
+                    res = ' ';
+                }
+                else{
+                    res = String.valueOf(board[i][j]).charAt(0);
+                }
+                System.out.print(res+ " | ");
+            }
+            System.out.println();
         }
     }
 
@@ -26,7 +54,7 @@ public class Minesweeper {
                 else{
                     res = String.valueOf(board[i][j]).charAt(0);
                 }
-                System.out.print(res);
+                System.out.print(res+ " | ");
             }
             System.out.println();
             printSeparator(board[0].length);
@@ -45,6 +73,7 @@ public class Minesweeper {
             for (int j = 0; j < cols; j++) {
                 if (Math.random() < mineProbability) {
                     board[i][j] = 100; // Mine
+                    mines++;
                 } else {
                     board[i][j] = 0; // Empty cell
                 }
@@ -109,6 +138,18 @@ public class Minesweeper {
     }
 
 
+    static int getExplored(boolean [][]explored){
+        int count = 0;
+        for(int i = 0;i<explored.length;i++){
+            for(int j = 0;j<explored[0].length;j++){
+                if(explored[i][j] == true){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -120,7 +161,10 @@ public class Minesweeper {
         int[][] board = generateMinesweeperBoard(rows, cols, mineProbability);
         int [][]resultantBoard = generateResultantBoard(board);
         printMinesweeperBoard(board);
-        printGameBoard(resultantBoard);
+        System.out.println("################################");
+        System.out.println("################################");
+        
+        printBoard(resultantBoard);
 
 
         int difficulty = 0;
@@ -130,33 +174,95 @@ public class Minesweeper {
         // Get difficulty from user
         difficulty = sc.nextInt();
         
-        while(difficulty < 0 || difficulty > 3){
-            System.out.println("Select the difficulty properly\n\n\n");
-            System.out.println("Enter the Difficulty Level You Want : 1 2 3");
-            difficulty = sc.nextInt();
-        }
+        // while(difficulty < 0 || difficulty > 3){
+        //     System.out.println("Select the difficulty properly\n\n\n");
+        //     System.out.println("Enter the Difficulty Level You Want : 1 2 3");
+        //     difficulty = sc.nextInt();
+        // }
 
-        //TODO:  Based on difficulty Change the board
+        // //TODO:  Based on difficulty Change the board
 
         int [][]gameBoard = new int[rows][cols];
-        printMinesweeperBoard(gameBoard);
+        boolean [][]explored = new boolean[rows][cols];
+        
         printGameBoard(gameBoard);
 
+        while(true){
 
-        
+            System.out.println("Enter the cell Number : ");
+            int row = sc.nextInt();
+            int col = sc.nextInt();
 
+            // System.out.println("Resultant Board");
+            // System.out.println(Arrays.deepToString(resultantBoard));
 
+            if(resultantBoard[row][col] == 100){
+                System.out.println("You Lost!!!");
+                break;
+            }
+            else if(resultantBoard[row][col] >= 1){
+                gameBoard[row][col] = resultantBoard[row][col];
+                explored[row][col] = true;
+            }
+            else{
+                updateGameBoard(gameBoard, row, col, new boolean[rows][cols], resultantBoard, explored);                
+            }
+            printBoard(gameBoard);
+            int noOfExplored = getExplored(explored);
+
+                System.out.println("No of Explored" + noOfExplored);
+
+                if(rows * cols - noOfExplored == mines){
+                    System.out.println("You Win!!!");
+                    break;
+                }
+
+        }
 
         System.out.println("You're good to go now");
 
+    }
 
+    static void updateGameBoard(int [][]gameBoard, int row, int col, boolean visited[][], int [][]resultantBoard, boolean [][]explored) {
 
+        // Go in all directions, 
+        // For every cell, check if the cell is a number, if it is, updat the gameBoard and return
+        // if the cell is not a number, mark it -1 and explore all the other cells around it
+        // make sure that you don't repeat the same cells and don't go out of bounds.
+        if(row < 0 || row >= gameBoard.length || col < 0 || col >= gameBoard[0].length){
+            return;
+        }
 
+        if(resultantBoard[row][col] == 100){
+            return;
+        }
+      
 
+        if(visited[row][col] == true){
+            return;
+        }
 
+        if(resultantBoard[row][col] >= 1){
+            gameBoard[row][col] = resultantBoard[row][col];
+            explored[row][col] = true;
+            return;
+        }
 
+        if(resultantBoard[row][col] == 0){
+            gameBoard[row][col] = -1;
+            explored[row][col] = true;
+        }
 
+        visited[row][col] = true;
 
-
+        // Explore all directions
+        updateGameBoard(gameBoard, row-1, col-1, visited, resultantBoard, explored);
+        updateGameBoard(gameBoard, row-1, col, visited, resultantBoard, explored);
+        updateGameBoard(gameBoard, row-1, col+1, visited, resultantBoard, explored);
+        updateGameBoard(gameBoard, row, col-1, visited, resultantBoard, explored);
+        updateGameBoard(gameBoard, row, col+1, visited, resultantBoard, explored);
+        updateGameBoard(gameBoard, row+1, col-1, visited, resultantBoard, explored);
+        updateGameBoard(gameBoard, row+1, col, visited, resultantBoard, explored);
+        updateGameBoard(gameBoard, row+1, col+1, visited, resultantBoard, explored);
     }
 }
